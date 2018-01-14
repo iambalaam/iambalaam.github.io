87c171aa6bb3d82b0a754287c4e192b79045bddb
Balaam <guybal@dca904870bc9.ant.amazon.com>
2018-01-14 17:08:16 +0000

    Development enhancements, hot-reload, built index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.tsx',
 
     devtool: 'inline-source-map',
+    devServer: {
+        hot: true
+    },
 
     module: {
         rules: [
@@ -16,9 +22,30 @@ module.exports = {
             {
                 test: /\.s?css$/,
                 use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: 'file-loader'
             }
+            // If I need to include fonts in my bundle
+            // {
+            //     test: /\.(woff|woff2|eot|ttf|otf)$/,
+            //     use: 'file-loader'
+            // }
         ]
     },
+
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'iambalaam',
+            inject: false,
+            template: require('html-webpack-template'),
+            appMountId: 'app'
+        })
+    ],
 
     resolve: {
         extensions: ['.tsx', '.ts', '.js']
